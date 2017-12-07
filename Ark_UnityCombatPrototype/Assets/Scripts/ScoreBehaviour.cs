e38ef434b5798013852089d76148464299d9ca2e
Juan Sánchez <juanman51@gmail.com>
2017-12-07 21:24:55 +0100

    Moved the combat prototype to another folder to be able to create other prototypes in the base directory

---
+++ Ark_UnityCombatPrototype/Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehaviour : MonoBehaviour {
+    public Text droneKillsText;
+
+    void Start () {
+        int droneKills = PlayerPrefs.GetInt("DroneKills");
+        droneKillsText.text = "YOU KILLED  " + droneKills.ToString() + "  DRONES";
+    }
+	
+	void Update () {
+	}
+}
---
+++ Ark_UnityCombatPrototype/Assets/Scripts/ShotBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShotBehaviour : MonoBehaviour {
+    private const float PLAYER_PROJECTILE_SPEED = 50f;
+    private const float EXPLOSION_TTL = 1f;
+
+    public GameObject explosionPrefab;
+
+    void Start () {
+    }
+	
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * PLAYER_PROJECTILE_SPEED;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "IsDrone")
+        {
+            Destroy(other.gameObject);
+            GameObject.FindObjectOfType<RangeAttackControl>().DroneKilled();
+            GameObject explosion = GameObject.Instantiate(explosionPrefab, gameObject.transform.position, transform.rotation) as GameObject;
+            GameObject.Destroy(explosion, EXPLOSION_TTL);
+        }
+
+        if (other.tag != "IsPlayer")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
